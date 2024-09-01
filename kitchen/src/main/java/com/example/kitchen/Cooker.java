afc1f41d8dabab1e56a77b0f6f2001b91026c7dc
Stefan Grozev <stefan.grozev@lights.digital>
2024-09-01 21:11:16 +0300

    add ingredients class

--- kitchen/src/main/java/com/example/kitchen/Cooker.java
+++ kitchen/src/main/java/com/example/kitchen/Cooker.java
@@ -1,14 +1,15 @@
 package com.example.kitchen;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.example.kitchen.exceptions.FailedToCookException;
 
 @Component
 public class Cooker {
+
+    @Autowired
+    Ingredients ingredients;
 
     public String cookRandomMeal() throws FailedToCookException {
         try {
@@ -21,8 +22,7 @@ public class Cooker {
     }
 
     private String getRandomMeal() {
-        List<String> ingredients = new ArrayList<>(List.of("CHICKEN READY", "BEEF READY", "LAST MONTH MAMMOTH READY"));
-        int meal = (int) (Math.random() * ingredients.size());
-        return ingredients.get(meal);
+        int meal = (int) (Math.random() * ingredients.getIngredients().size());
+        return ingredients.getIngredients().get(meal);
     }
 }
---
+++ kitchen/src/main/java/com/example/kitchen/Ingredients.java
@@ -0,0 +1,17 @@
+package com.example.kitchen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Component
+public class Ingredients {
+    private final List<String> ingredients = new ArrayList<>(List.of("CHICKEN READY", "BEEF READY", "LAST MONTH MAMMOTH READY"));
+
+}
