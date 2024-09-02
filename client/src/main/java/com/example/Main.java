ef5af954d0e78e47d79722fda627fc85f882107b
Stefan Grozev <stefan.grozev@lights.digital>
2024-09-02 21:58:57 +0300

    handle proxy connection

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -2,12 +2,26 @@ package com.example;
 
 import java.util.concurrent.CountDownLatch;
 
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.ProxyProvider;
 
 public class Main {
 
     public static void main(String[] args) {
-        WebClient webClient = WebClient.create("http://127.0.0.1:8080");
+        HttpClient httpClient = HttpClient.create()
+                .proxy(proxy -> proxy
+                .type(ProxyProvider.Proxy.HTTP)
+                .host("localhost")
+                .port(3128));
+
+        ReactorClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+
+        WebClient webClient = WebClient.builder()
+        .clientConnector(connector)
+        .baseUrl("http://127.0.0.1:8080").build();
         CountDownLatch latch = new CountDownLatch(1);
 
         KitchenClient kitchenClient = new KitchenClient(webClient, latch);
